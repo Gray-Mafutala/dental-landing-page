ea6336621f5637f2e6f2158a40ac093219d2e898
Gray Mafutala <graymafutala@gmail.com>
2024-02-19 22:22:44 +0300

    Updates

---
+++ assets/article-cover-3-El2mV4Za.js
@@ -0,0 +1,1 @@
+const e="/dental-landing-page/assets/article-cover-1-UeHdp_5T.png",a="/dental-landing-page/assets/article-cover-2-SB0x1iMX.png",t="/dental-landing-page/assets/article-cover-3-9-K--hK_.png";export{e as A,a,t as b};
--- assets/index-ZXXCcsGp.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-WzT_Z-Fb.js";const e=()=>t.jsx(t.Fragment,{children:"404 - Page not found"});export{e as default};
